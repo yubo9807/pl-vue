650b2f1477fe5160200e064c6367a6c0f12bbd87
yubo9807 <yubo9807@gmail.com>
2023-03-17 09:39:29 +0800

    feat: 深度比较两个值是否相同

--- src/utils/judge.ts
+++ src/utils/judge.ts
@@ -3,6 +3,7 @@ import { Key } from "./type";
 export type Type = 'string'    | 'number'  | 'boolean' |
                    'symbol'    | 'bigint'  |
                    'undefined' | 'null'    |
+                   'regexp'    | 'date'    |
                    'array'     | 'object'  |
                    'function'  | 'promise' |
                    'set'       | 'map'     |
@@ -32,4 +33,24 @@ export function isObject(o: any) {
  */
 export function hasOwn(target: object | any[], key: Key) {
   return Object.prototype.hasOwnProperty.call(target, key);
+}
+
+/**
+ * 判断两个值是否相等
+ * @param val1 
+ * @param val2 
+ * @returns 
+ */
+export function isEquals(val1: any, val2: any) {
+  if (typeof val1 === 'object' && typeof val2 === 'object') {
+    const keys1 = Object.keys(val1), keys2 = Object.keys(val2);
+    if (keys1.length !== keys2.length) return false;
+    for (const key of keys1) {
+      if (!keys2.includes(key)) return false;
+      return isEquals(val1[key], val2[key]);
+    }
+    return true;
+  } else {
+    return val1 === val2;
+  }
 }
