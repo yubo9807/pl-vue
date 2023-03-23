db68c68e7df534970152ef6f2b6895f7b2152f79
yubo9807 <yubo9807@gmail.com>
2023-03-23 09:11:04 +0800

    feat: webpack build module scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { h, ref, Fragment, render } from "./vue3";
-
+import style from './module.scss';
 
 function App() {
   const count = ref(1);
   return <>
     <Comp text="word" count={() => count.value} />
     {/* 响应式数据写为函数形式 */}
-    <h1>{() => count.value}</h1>
+    <h1 className={style.demo}>{() => count.value}</h1>
     <button onclick={() => count.value ++}>click</button>
   </>
 }
@@ -16,10 +16,10 @@ type CompProps = {
   count: () => number  // 想让父组件传递的 props 具有响应式也同样传一个函数类型
 }
 function Comp(props: CompProps) {
-  return <>
+  return <div>
     hello {props.text}
     {props.count}
-  </>
+  </div>
 }
 
 document.getElementById('root').appendChild(render(<App />));
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.tsx',
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].[fullhash:8].js'
+  },
+  mode: 'production',
+  // devtool: "source-map",
+  watch: false,
+  resolve: {
+    extensions: [".ts", ".tsx"],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]__[chunkhash:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ]
+      },
+      {
+        test: /.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[fullhash:8].css',
+    }),
+  ],
+}
