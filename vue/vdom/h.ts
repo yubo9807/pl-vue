556eeac4ecf87e010fca6e5238fa2856127fdb54
yubo9807 <yubo9807@gmail.com>
2023-04-12 11:28:18 +0800

    feat: high orderc component attrs transfer

--- vue/vdom/h.ts
+++ vue/vdom/h.ts
@@ -3,14 +3,23 @@ import { Attrs, Children, Tag } from "./type";
 import { isComponent } from "./utils";
 
 export function h(tag: Tag, attrs: Attrs, ...children: Children) {
-  if (isComponent(tag)) {
-    (tag as Function).prototype._id = createId();
-  }
-  return {
+  const tree = {
     tag,
     attrs: attrs || {},
     children,
   }
+
+  // 对组件做一些处理
+  if (isComponent(tree.tag)) {
+    (tree.tag as Function).prototype._id = createId();
+
+    // 高阶组件 props 传递
+    if (tree.children.length === 0 && tree.attrs.children) {
+      tree.children = tree.attrs.children;
+    }
+  }
+
+  return tree;
 }
 
 export function Fragment({ children }) {
