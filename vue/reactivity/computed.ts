4aa5b1cf14c69314e7bc6d8f8f675fe84db64f46
yubo9807 <yubo9807@gmail.com>
2023-04-30 14:24:49 +0800

    style: computed type

--- vue/reactivity/computed.ts
+++ vue/reactivity/computed.ts
@@ -1,7 +1,14 @@
-import { ISREF, ref } from "./ref";
+import { ISREF } from "./ref";
 import { ReactiveEffect } from './effect'
 
-class ComputedRefImpl {
+type Getter<T> = () => T
+type Setter<T> = (val: T) => void
+type ComputedOption<T> = Getter<T> | {
+  get: Getter<T>
+  set: Setter<T>
+}
+
+class ComputedRefImpl<T> {
 
   __v_isReadonly = true;
   [ISREF]        = true;
@@ -11,32 +18,28 @@ class ComputedRefImpl {
   computed: ReactiveEffect
   _setter:  Function
 
-  constructor(getter: Function, setter?: Function) {
+  constructor(getter: Getter<T>, setter?: Setter<T>) {
     this.computed = new ReactiveEffect(getter);
     this._setter  = setter;
   }
 
-  get value() {
+  get value(): T {
     return this.computed.fn();
   }
 
-  set value(val) {
+  set value(val: T) {
     this._setter ? this._setter(val) : console.warn(`Write operation failed: computed value is readonly`);
   }
 
 }
 
-type ComputedOption = Function | {
-  get: Function
-  set: Function
-}
 
 /**
  * 计算属性
  * @param option 
  * @returns 
  */
-export function computed(option: ComputedOption) {
+export function computed<T>(option: ComputedOption<T>) {
   if (typeof option === 'function') {
     return new ComputedRefImpl(option);
   }
