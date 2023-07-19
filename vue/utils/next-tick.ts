6fe7c6ba640058150c14b469813a5437e9f45e79
yubo9807 <yubo9807@gmail.com>
2023-07-19 09:13:25 +0800

    style: nextTick simplify

--- vue/utils/next-tick.ts
+++ vue/utils/next-tick.ts
@@ -4,16 +4,21 @@
  * @param func 
  */
 export function nextTick(func: Function) {
-  if (typeof Promise !== void 0) {
-    Promise.resolve().then(func as any);
-  } else if (typeof MutationObserver !== void 0) {
-    const ob = new MutationObserver(func as MutationCallback);
-    const textNode = document.createTextNode('0');
-    ob.observe(textNode, { characterData: true });
-    textNode.data = '1';
-  } else if (typeof process !== void 0) {
-    process.nextTick(func);
-  } else {
-    setTimeout(func, 0);
-  }
+  Promise.resolve().then(func as any);
 }
+
+// 兼容各种环境
+// export function nextTick(func: Function) {
+//   if (typeof Promise !== void 0) {
+//     Promise.resolve().then(func as any);
+//   } else if (typeof MutationObserver !== void 0) {
+//     const ob = new MutationObserver(func as MutationCallback);
+//     const textNode = document.createTextNode('0');
+//     ob.observe(textNode, { characterData: true });
+//     textNode.data = '1';
+//   } else if (typeof process !== void 0) {
+//     process.nextTick(func);
+//   } else {
+//     setTimeout(func, 0);
+//   }
+// }
