e66a2bfe005b9c2adac51303953ad81b49bb46b6
yubo9807 <yubo9807@gmail.com>
2023-07-19 12:13:21 +0800

    feat: vite

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './')
+    }
+  },
+  css: {
+    modules: {
+      generateScopedName: '[local]-[hash:6]',
+      localsConvention: 'camelCase',
+      scopeBehaviour: 'local',
+    },
+  },
+})
