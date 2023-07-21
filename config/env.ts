0a56f74672c0e59d4cc071b1795891505c5c6f6c
yubo9807 <yubo9807@gmail.com>
2023-07-21 09:12:10 +0800

    chore: config

--- config/env.ts
+++ config/env.ts
@@ -1,4 +1,14 @@
 
+type Mode = 'development' | 'production'
+
 export default {
+
   BASE_URL: '/',
+
+  MODE: (import.meta as any).env.MODE as Mode,
+
+  SSR: (import.meta as any).env.SSR as boolean,
+
+  BASE_API: '/api',
+
 }
